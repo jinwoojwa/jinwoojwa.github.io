d54ed59594b406dc0dab4d8cde4765d0e3ea8d9a
jinwoo <jjw6654@gmail.com>
2026-05-17 14:01:15 +0900

    chore: sitemap 설정 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
+  site: {
+    url: 'https://jinwoojwa.github.io',
+  },
+
   app: {
     head: {
       link: [
